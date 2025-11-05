4849e5145f88b0016c1f716455244656e9ba63e7
fanyonglong <315144604@qq.com>
2025-11-06 07:42:27 +0800

    'f'

---
+++ packages/fabric/extensions/index.ts
@@ -0,0 +1,14 @@
+export {
+  initAligningGuidelines,
+  type AligningLineConfig,
+} from './aligning_guidelines';
+
+export {
+  originUpdaterWrapper,
+  installOriginWrapperUpdater,
+} from './data_updaters/origins';
+
+export {
+  gradientUpdaterWrapper,
+  installGradientUpdater,
+} from './data_updaters/gradient';
