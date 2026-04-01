015f09c44258d415f2261487968b016b8bdba68c
fanyonglong <315144604@qq.com>
2026-04-02 01:02:46 +0800

    f

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -2,5 +2,5 @@ export * as snabbdom from './snabbdom'
 export * as inferno from './infernos/inferno'
 export * as React from './react16/react'
 export * as ReactDom from './react16/react-dom'
-
+export * from './jquery/jquery'
 
